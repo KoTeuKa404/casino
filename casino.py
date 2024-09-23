b88912c3f1451f90d9935ff20941dbda1e6a5cb6
KoTeuKa404 <135438716+KoTeuKa404@users.noreply.github.com>
2024-09-23 22:19:59 +0300

    Initial commit

---
+++ casino.py
@@ -0,0 +1,132 @@
+import pygame
+import sys
+import slots   
+import tiles   
+import poker
+import match_3  
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+screen_width = 1000
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)  
+pygame.display.set_caption("Casino Games")
+
+font = pygame.font.Font(None, 36)
+
+background = pygame.image.load('img/background.png')  
+background = pygame.transform.scale(background, (screen_width, screen_height))   
+
+slot_image = pygame.image.load('img/game1.png')  
+slot_image = pygame.transform.scale(slot_image, (150, 150))  
+
+poker_image = pygame.image.load('img/poker.png') 
+poker_image = pygame.transform.scale(poker_image, (150, 150))
+
+tiles_image = pygame.image.load('img/tiles.png')  
+tiles_image = pygame.transform.scale(tiles_image, (150, 150))
+
+match_3_image = pygame.image.load('img/match_3.png')
+match_3_image = pygame.transform.scale(match_3_image, (150, 150))
+
+def draw_text(text, font, color, surface, x, y):
+    text_obj = font.render(text, True, color)
+    text_rect = text_obj.get_rect()
+    text_rect.center = (x, y)
+    surface.blit(text_obj, text_rect)
+
+class CasinoGame:
+    def __init__(self):
+        self.running = True
+
+    def game_loop(self):
+        global screen, screen_width, screen_height, background  
+        offset = 200  
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                
+                if event.type == pygame.VIDEORESIZE:
+                    screen_width, screen_height = event.w, event.h
+                    screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
+                  
+                    background = pygame.transform.scale(pygame.image.load('img/background.png'), (screen_width, screen_height))
+
+           
+            screen.blit(background, (0, 0))   
+            draw_text("Casino Games", font, BLACK, screen, screen_width // 2, 50)
+
+          
+            pygame.draw.rect(screen, BLACK, (200 + offset, 80, 150, 150), 2)   
+            pygame.draw.rect(screen, BLACK, (450 + offset, 80, 150, 150), 2)  
+            pygame.draw.rect(screen, BLACK, (200 + offset, 280, 150, 150), 2)  
+            pygame.draw.rect(screen, BLACK, (450 + offset, 280, 150, 150), 2)  
+
+            screen.blit(slot_image, (450 + offset, 80))
+            screen.blit(poker_image, (200 + offset, 80))
+            screen.blit(tiles_image, (200 + offset, 280))  
+            screen.blit(match_3_image, (450 + offset, 280))  
+
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
+
+            
+            if 200 + offset < mouse[0] < 350 + offset and 230 < mouse[1] < 280:
+                pygame.draw.rect(screen, BLUE, (200 + offset, 230, 150, 50))
+                if click[0] == 1:
+                    self.running = False  
+                    poker.poker_game() 
+            else:
+                pygame.draw.rect(screen, GREEN, (200 + offset, 230, 150, 50))
+
+            draw_text("Poker", font, WHITE, screen, 275 + offset, 255)
+
+          
+            if 450 + offset < mouse[0] < 600 + offset and 230 < mouse[1] < 280:
+                pygame.draw.rect(screen, BLUE, (450 + offset, 230, 150, 50))
+                if click[0] == 1:
+                    self.running = False 
+                    slots.slots_game()   
+            else:
+                pygame.draw.rect(screen, GREEN, (450 + offset, 230, 150, 50))
+
+            draw_text("Slots", font, WHITE, screen, 525 + offset, 255)
+
+           
+            if 200 + offset < mouse[0] < 350 + offset and 430 < mouse[1] < 480:
+                pygame.draw.rect(screen, BLUE, (200 + offset, 430, 150, 50))
+                if click[0] == 1:
+                    self.running = False  
+                    tiles.tiles_game()  
+            else:
+                pygame.draw.rect(screen, GREEN, (200 + offset, 430, 150, 50))
+
+            draw_text("Tiles", font, WHITE, screen, 275 + offset, 455)
+
+           
+            if 450 + offset < mouse[0] < 600 + offset and 430 < mouse[1] < 480:
+                pygame.draw.rect(screen, BLUE, (450 + offset, 430, 150, 50))
+                if click[0] == 1:
+                    self.running = False  
+                    match_3.match_3_game()  
+            else:
+                pygame.draw.rect(screen, GREEN, (450 + offset, 430, 150, 50))
+
+            draw_text("Match-3", font, WHITE, screen, 525 + offset, 455)
+
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    game = CasinoGame()
+    game.game_loop()
---
+++ poker.py
@@ -0,0 +1,203 @@
+import pygame
+import random
+import sys
+import os
+
+pygame.init()
+pygame.mixer.init()  
+
+CARD_WIDTH, CARD_HEIGHT = 100, 150
+CARD_SPACING = 25
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 128, 0)
+BLACK = (0, 0, 0)
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("Poker Game 1-on-1")
+
+font = pygame.font.Font(None, 36)
+winner_font = pygame.font.Font(None, 60)
+
+SUITS = ['hearts', 'diamonds', 'clubs', 'spades']
+RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+player_chips = 1000
+ai_chips = 1000
+pot = 0
+current_bet = 0
+community_cards = []
+hands = {}
+game_phase = 'pre-flop'
+winner_text = ""
+
+card_images = {}
+for suit in SUITS:
+    for rank in RANKS:
+        card_images[f"{rank}_of_{suit}"] = pygame.image.load(f"img/card/{rank}_of_{suit}.png")
+
+card_back_image = pygame.image.load("img/card/back.png")
+
+music_folder = "music/radio" 
+music_files = [os.path.join(music_folder, file) for file in os.listdir(music_folder) if file.endswith(".ogg")]
+
+def play_random_music():
+    if music_files:
+        random_music = random.choice(music_files)
+        pygame.mixer.music.load(random_music)
+        pygame.mixer.music.play()
+
+def create_deck():
+    deck = [{'rank': rank, 'suit': suit} for suit in SUITS for rank in RANKS]
+    random.shuffle(deck)
+    return deck
+
+def deal_cards(deck):
+    return {'Player': [deck.pop(), deck.pop()], 'AI': [deck.pop(), deck.pop()]}
+
+def draw_card(card, x, y):
+    card_key = f"{card['rank']}_of_{card['suit']}"
+    scaled_card = pygame.transform.scale(card_images[card_key], (CARD_WIDTH, CARD_HEIGHT))
+    screen.blit(scaled_card, (x, y))
+
+def draw_community_cards(cards):
+    community_card_y = SCREEN_HEIGHT // 2 - CARD_HEIGHT // 2
+    for i, card in enumerate(cards):
+        x_position = 450 + (CARD_WIDTH + CARD_SPACING + 20) * i
+        draw_card(card, x_position, community_card_y)
+
+def draw_player_hands(hands):
+    player_hand_y = SCREEN_HEIGHT - CARD_HEIGHT - 150
+    for i, card in enumerate(hands['Player']):
+        x_position = 100 + (CARD_WIDTH + CARD_SPACING + 20) * i
+        draw_card(card, x_position, player_hand_y)
+
+def draw_ai_hands(ai_hand, reveal=False):
+    ai_hand_y = 100
+    for i, card in enumerate(ai_hand):
+        x_position = 100 + (CARD_WIDTH + CARD_SPACING + 20) * i
+        if reveal:
+            draw_card(card, x_position, ai_hand_y)
+        else:
+            scaled_back = pygame.transform.scale(card_back_image, (CARD_WIDTH, CARD_HEIGHT))
+            screen.blit(scaled_back, (x_position, ai_hand_y))
+
+def create_button(x, y, w, h, text):
+    pygame.draw.rect(screen, BLUE, (x, y, w, h))
+    text_surf = font.render(text, True, WHITE)
+    screen.blit(text_surf, (x + w // 4, y + h // 4))
+
+def determine_winner(player_hand, ai_hand, community_cards):
+    player_score = sum([RANKS.index(card['rank']) for card in player_hand + community_cards])
+    ai_score = sum([RANKS.index(card['rank']) for card in ai_hand + community_cards])
+    
+    if player_score > ai_score:
+        return "Player Wins!"
+    elif ai_score > player_score:
+        return "AI Wins!"
+    else:
+        return "It's a Draw!"
+
+def poker_game():
+    global player_chips, ai_chips, pot, current_bet, community_cards, hands, game_phase, SCREEN_WIDTH, SCREEN_HEIGHT, screen, winner_text
+
+    deck = create_deck()
+    hands = deal_cards(deck)
+    community_cards = [deck.pop() for _ in range(5)]
+    showdown = False
+
+    running = True
+    num_community_cards = 0
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.VIDEORESIZE:
+                SCREEN_WIDTH, SCREEN_HEIGHT = event.size
+                screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+
+                if 50 <= mouse_x <= 200 and SCREEN_HEIGHT - 100 <= mouse_y <= SCREEN_HEIGHT - 50:
+                    if player_chips >= 100:
+                        current_bet = 100
+                        player_chips -= current_bet
+                        pot += current_bet
+                        num_community_cards = 3 
+
+                elif 250 <= mouse_x <= 400 and SCREEN_HEIGHT - 100 <= mouse_y <= SCREEN_HEIGHT - 50:
+                    if player_chips >= current_bet:
+                        player_chips -= current_bet
+                        pot += current_bet
+                        num_community_cards += 1 
+                        if num_community_cards == 5:
+                            showdown = True 
+
+                elif 450 <= mouse_x <= 600 and SCREEN_HEIGHT - 100 <= mouse_y <= SCREEN_HEIGHT - 50:
+                    if player_chips >= current_bet + 200:
+                        current_bet += 200
+                        player_chips -= current_bet
+                        pot += current_bet
+                        num_community_cards = 5  
+                        showdown = True
+
+                elif 650 <= mouse_x <= 800 and SCREEN_HEIGHT - 100 <= mouse_y <= SCREEN_HEIGHT - 50:
+                    ai_chips += pot 
+                    pot = 0
+                    deck = create_deck()
+                    hands = deal_cards(deck)
+                    community_cards = [deck.pop() for _ in range(5)]
+                    current_bet = 0
+                    num_community_cards = 0
+                    showdown = False
+
+        screen.fill(GREEN)
+        draw_player_hands(hands)
+        draw_community_cards(community_cards[:num_community_cards])
+
+        if showdown:
+            draw_ai_hands(hands['AI'], reveal=True)  
+            winner_text = determine_winner(hands['Player'], hands['AI'], community_cards)
+            print(winner_text)
+            if winner_text == "Player Wins!":
+                player_chips += pot
+            elif winner_text == "AI Wins!":
+                ai_chips += pot
+            pot = 0
+
+            text_surf = winner_font.render(winner_text, True, WHITE)
+            screen.blit(text_surf, (SCREEN_WIDTH // 2 - 120, SCREEN_HEIGHT // 2 - 180))
+
+            pygame.display.update()
+            pygame.time.wait(3000)  
+
+            deck = create_deck()
+            hands = deal_cards(deck)
+            community_cards = [deck.pop() for _ in range(5)]
+            num_community_cards = 0
+            showdown = False
+        else:
+            draw_ai_hands(hands['AI']) 
+
+        create_button(50, SCREEN_HEIGHT - 100, 150, 50, "Bet")
+        create_button(250, SCREEN_HEIGHT - 100, 150, 50, "Call")
+        create_button(450, SCREEN_HEIGHT - 100, 150, 50, "Raise")
+        create_button(650, SCREEN_HEIGHT - 100, 150, 50, "Fold")
+
+        text_surf = font.render(f"Player Chips: {player_chips}   Pot: {pot}   AI Chips: {ai_chips}", True, WHITE)
+        screen.blit(text_surf, (SCREEN_WIDTH // 2 - 300, 50))
+
+        pygame.display.update()
+
+        if not pygame.mixer.music.get_busy():
+            play_random_music()
+
+
+if __name__ == "__main__":
+    play_random_music()  
+    poker_game()
